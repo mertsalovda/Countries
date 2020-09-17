9840b3a2cb61cba6277934e0f6b610601ac66772
mertsalovda <korranhorn009@gmail.com>
2020-09-17 11:57:28 +0300

    Реализована extension функция, представляющая коллекцию в виде колонки.

---
+++ app/src/main/java/ru/mertsalovda/countries/extensions/List.kt
@@ -0,0 +1,14 @@
+package ru.mertsalovda.countries.extensions
+
+/**
+ * Получить коллекцию в виде строки, где элементы коллекции расопложены в виде колонки.
+ *
+ * @return строка без прямоугольных скобок и запятие заменены символом переноса строки.
+ */
+fun List<Any>.printInColumn(): String {
+    return this.toString()
+        .replace("[", "")
+        .replace("]", "")
+        .replace(", ", "\n")
+        .trim()
+}
