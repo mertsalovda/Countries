a3682956e55ccb139ea8866c804c90287544e44a
mertsalovda <korranhorn009@gmail.com>
2020-09-17 11:56:39 +0300

    Изменён GET-запрос к серверу для получения объекта с точным совпадением name.

--- app/src/main/java/ru/mertsalovda/countries/repositories/api/CountriesApi.kt
+++ app/src/main/java/ru/mertsalovda/countries/repositories/api/CountriesApi.kt
@@ -2,6 +2,7 @@ package ru.mertsalovda.countries.repositories.api
 
 import io.reactivex.Observable
 import retrofit2.http.GET
+import retrofit2.http.Path
 import ru.mertsalovda.countries.models.data.Country
 
 interface CountriesApi {
@@ -9,6 +10,6 @@ interface CountriesApi {
     @GET("all?fields=name;flag;currencies;languages;timezones")
     fun getAllCountries() : Observable<List<Country>>
 
-    @GET("name/rico?fields=name;flag;currencies;languages;timezones")
-    fun getPuertoRico() : Observable<List<Country>>
+    @GET("name/{name}?fullText=true;flag;currencies;languages;timezones")
+    fun getCountryByName(@Path("name") name: String) : Observable<List<Country>>
 }
